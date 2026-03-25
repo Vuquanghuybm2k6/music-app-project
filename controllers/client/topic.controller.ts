58b62be41243b98d0da866670b085494a23b211a
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-03-25 10:25:47 +0700

    Build music genres page

--- controllers/client/topic.controller.ts
+++ controllers/client/topic.controller.ts
@@ -1,8 +1,17 @@
 import {Request, Response} from 'express'
+import Topic from '../../models/topic.model'
 
-export const index = (req:Request, res:Response) => {
-
-  res.render('client/pages/topics/index',{
-    pageTitle: "Chủ đề bài hát",
-  })
+export const index = async (req:Request, res:Response) => {
+  try {
+    const topics = await Topic.find({
+      deleted: false  
+    })
+    res.render('client/pages/topics/index',{
+      pageTitle: "Chủ đề bài hát",
+      topics: topics
+    })
+  } catch (error) {
+    console.error('Error fetching topics:', error)
+    res.status(500).send('Internal Server Error')
+  }
 }
