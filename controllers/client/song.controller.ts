96cf7ceb86bcfab13153902fa253401647f68377
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-03-25 23:42:13 +0700

    Build song detail page

--- controllers/client/song.controller.ts
+++ controllers/client/song.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express"
 import Topic from "../../models/topic.model"
 import Song from "../../models/song.model"
 import Singer from "../../models/singer.model"
+
 // [GET]: /songs/:slugTopic
 export const list = async (req: Request, res: Response) => {
   const topic = await Topic.findOne({
@@ -9,7 +10,6 @@ export const list = async (req: Request, res: Response) => {
     status: "active",
     deleted: false
   })
-
   if (!topic) {
     return res.status(404).send("Topic not found");
   }
@@ -30,5 +30,37 @@ export const list = async (req: Request, res: Response) => {
   res.render("client/pages/songs/list.pug",{
     pageTitle: topic.title,
     songs
+  })
+}
+
+// [GET]: /songs/detail/:slugSong
+export const detail = async (req:Request, res: Response)=>{
+  const slugSong = req.params.slugSong
+  const song = await Song.findOne({
+    slug: slugSong, 
+    deleted: false,
+    status: "active"  
+  }).select("avatar title slug singerId topicId like")
+  if(!song){
+    return res.status(404).send("Song not found")
+  }
+  console.log(song)
+  const singer = await Singer.findOne({
+    _id: song.singerId,
+    deleted: false,
+    status: "active"
+  }).select("fullName")
+  console.log(singer)
+  const topic = await Topic.findOne({
+    _id: song.topicId,
+    deleted: false,
+    status: "active"  
+  })
+  console.log(topic)
+  res.render("client/pages/songs/detail.pug",{
+    pageTitle: song.title,
+    song,
+    singer,
+    topic
   })
 }
