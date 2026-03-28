0a464be6debe8e6062c717ba5bfa3fb00285fc60
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-03-28 12:30:55 +0700

    Buil search suggestion feature

--- controllers/client/search.controller.ts
+++ controllers/client/search.controller.ts
@@ -2,12 +2,14 @@ import { Request, Response } from "express"
 import Song from "../../models/song.model"
 import Singer from "../../models/singer.model"
 import { convertToSlug } from "../../helpers/convertToSlug"
-// [GET]: /search/result
+
+// [GET]: /search/:type
 export const result = async (req: Request, res: Response) => {
+  const type = req.params.type
   const keyword:string = `${req.query.keyword}`
-  let newSongs: any[] = []
+  let newSongs = []
   if(keyword){
-    const keywordRegex = new RegExp(keyword, "i") // tạo regex để tìm kiếm không phân biệt hoa thường
+    const keywordRegex = new RegExp(keyword, "i")
     // Tạo ra slug không dấu, có thêm dấu - ngăn cách
     const stringSlug = convertToSlug(keyword)
     const stringSlugRegex = new RegExp(stringSlug, "i")
@@ -21,14 +23,35 @@ export const result = async (req: Request, res: Response) => {
     for(const item of songs){
       const infoSinger = await Singer.findOne({
         _id: item.singerId
-      });
-      (item as any)["infoSinger"] = infoSinger
+      })
+      newSongs.push({
+        id: item.id,
+        title: item.title,
+        avatar: item.avatar,
+        like: item.like,
+        slug: item.slug,
+        infoSinger: {
+          fullName: infoSinger?.fullName,
+        }
+      })
     }
-    newSongs = songs
   }
-  res.render("client/pages/search/result.pug",{
-    pageTitle: `Kết quả: ${keyword}`,
-    keyword,
-    songs: newSongs
-  })
+  switch (type){
+    case "result":
+      res.render("client/pages/search/result.pug",{
+        pageTitle: `Kết quả: ${keyword}`,
+        keyword,
+        songs: newSongs
+      })
+      break
+    case "suggest":
+      res.json({
+        code: 200,
+        message: "Tìm kiếm thành công",
+        songs: newSongs
+      })
+      break
+    default: 
+      break
+  }
 }
